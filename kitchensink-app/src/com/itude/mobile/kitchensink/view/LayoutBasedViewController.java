8b7a4c3dbb1a2809e1db90d4747ae545afe8526a
w.elsinga@itude.com <w.elsinga@itude.com>
2013-10-15 08:23:51 +0200

    Added license files and header information to all classes

--- kitchensink-app/src/com/itude/mobile/kitchensink/view/LayoutBasedViewController.java
+++ kitchensink-app/src/com/itude/mobile/kitchensink/view/LayoutBasedViewController.java
@@ -1,3 +1,18 @@
+/*
+ * (C) Copyright ItudeMobile.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.itude.mobile.kitchensink.view;
 
 import android.view.LayoutInflater;
@@ -27,12 +42,12 @@ public class LayoutBasedViewController extends MBBasicViewController
   {
     ViewGroup view = (ViewGroup) inflater.inflate(R.layout.layout_based_view, null);
 
-    _labelField = (MBField) getPage().getFirstDescendantOfKindWithName(MBField.class, "inputfield");
+    _labelField = getPage().getFirstDescendantOfKindWithName(MBField.class, "inputfield");
     String labelText = _labelField.getLabel();
     _label = (TextView) view.findViewById(R.id.label);
     _label.setText(labelText);
 
-    _buttonField = (MBField) getPage().getFirstDescendantOfKindWithName(MBField.class, "button");
+    _buttonField = getPage().getFirstDescendantOfKindWithName(MBField.class, "button");
     String buttonText = _buttonField.getLabel();
     _button = (Button) view.findViewById(R.id.button);
     _button.setText(buttonText);
