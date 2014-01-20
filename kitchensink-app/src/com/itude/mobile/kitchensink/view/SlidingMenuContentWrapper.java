3f82b6aa11db0ae1ad96ef5822ecbe36fe8825a3
Pjotter Tommassen <p.tommassen@itude.com>
2014-01-20 11:38:36 +0100

    - finished example drawer layout

--- kitchensink-app/src/com/itude/mobile/kitchensink/view/SlidingMenuContentWrapper.java
+++ kitchensink-app/src/com/itude/mobile/kitchensink/view/SlidingMenuContentWrapper.java
@@ -4,40 +4,88 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.DrawerLayout.LayoutParams;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ListView;
 
+import com.itude.mobile.android.util.AssertUtil;
+import com.itude.mobile.mobbl.core.controller.MBDialogController;
+import com.itude.mobile.mobbl.core.controller.MBDialogManager;
+import com.itude.mobile.mobbl.core.controller.MBDialogManager.MBDialogChangeListener;
 import com.itude.mobile.mobbl.core.controller.MBViewManager;
+import com.itude.mobile.mobbl.core.controller.MBViewManager.HomeButtonHandler;
 import com.itude.mobile.mobbl.core.view.builders.MBContentViewWrapper;
 
 public class SlidingMenuContentWrapper implements MBContentViewWrapper
 {
 
-  @Override
-  public View buildContentView(MBViewManager viewManager, int emplacementId)
+  public class SlidingMenuHomeButtonHandler implements HomeButtonHandler
   {
+
+    private final DrawerLayout _drawerLayout;
+
+    public SlidingMenuHomeButtonHandler(DrawerLayout drawerLayout)
+    {
+      _drawerLayout = drawerLayout;
+    }
+
+    @Override
+    public void handleButton()
+    {
+      MBViewManager viewManager = MBViewManager.getInstance();
+
+      if (viewManager == null)
+      {
+        return;
+      }
+
+      if (_drawerLayout.isDrawerVisible(GravityCompat.START)) _drawerLayout.closeDrawer(GravityCompat.START);
+      else _drawerLayout.openDrawer(GravityCompat.START);
+
+    }
+
+  }
+
+  @Override
+  public View buildContentView(MBViewManager viewManager, View container)
+  {
+
+    // creates the drawer layout (duh...)
+    final DrawerLayout wrapper = new DrawerLayout(viewManager);
+
+    // add the main container to the layout
     LayoutParams layout = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-
-    DrawerLayout wrapper = new DrawerLayout(viewManager);
-
-    FrameLayout container = new FrameLayout(viewManager);
-
-    container.setId(emplacementId);
-
-    LayoutParams drawerLayout = new LayoutParams(480, LayoutParams.MATCH_PARENT, GravityCompat.START);
-    ListView drawer = new ListView(viewManager);
-
-    drawer.setBackgroundColor(viewManager.getResources().getColor(android.R.color.holo_green_light));
-
     wrapper.addView(container, layout);
-    wrapper.addView(drawer, drawerLayout);
+
+    // grab the dialog containing the menu
+    MBDialogManager dialogManager = MBViewManager.getInstance().getDialogManager();
+    MBDialogController menu = dialogManager.getMenuDialog();
+
+    AssertUtil.notNull("menu", menu);
+
+    // needed to fill the menu; can probably also be done when the menu actually opens
+    menu.activateWithoutSwitching();
+
+    // add a background to the menu, since it defaults to being transparent; looks quite cool, but is probably not what you want ;)
+    int color = viewManager.getResources().getColor(android.R.color.background_light);
+    View menuView = menu.getMainContainer();
+    menuView.setBackgroundColor(color);
+
+    // finally, add the menu to the drawerlayout
+    LayoutParams drawerLayout = new LayoutParams(480, LayoutParams.MATCH_PARENT, GravityCompat.START);
+    wrapper.addView(menuView, drawerLayout);
+
+    // allow the menu to open when the home button in the action bar is pressed
+    viewManager.setHomeButtonHandler(new SlidingMenuHomeButtonHandler(wrapper));
+
+    // close the menu when a new dialog is activated (e.g. by switching to it from the menu ;))
+    dialogManager.addDialogChangeListener(new MBDialogChangeListener()
+    {
+
+      @Override
+      public void onDialogSelected(String name)
+      {
+        wrapper.closeDrawers();
+      }
+    });
 
     return wrapper;
-
-    /*    DrawerLayout drawer = (DrawerLayout) viewManager.getLayoutInflater().inflate(R.layout.main, null);
-
-        View content = drawer.findViewById(R.id.content_frame);
-        content.setId(emplacementId);
-        return drawer;*/
   }
 }
